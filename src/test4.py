4eb61705c5f02ef0100d5d9d232f47535ffd1f09
Alve Svarén <alve@hotmail.se>
2021-01-31 21:09:39 +0100

    Sending message in test4

--- src/test4.py
+++ src/test4.py
@@ -1,18 +1,29 @@
-from api.endpoints import get_channel
+from datetime import datetime
+from api.endpoints import create_message, get_channel
 from PySide2.QtCore import QCoreApplication
 import sys
 
+from api.interfaces import Snowflake
+
 app = QCoreApplication(sys.argv)
 
+requests_left = 2
 
 def handle_response(data, info):
+    global requests_left
+    requests_left -= 1
     print(data, info)
-    app.quit()
+    print("="*100)
+    if not requests_left: app.quit()
 
 def catch_err(error, info):
-    app.quit()
+    global requests_left
+    requests_left -= 1
+    if not requests_left: app.quit()
     raise error
 
 get_channel(574383922167808003).then(handle_response).catch(catch_err)
+create_message(764029277863280662, {"content": "hejsan", "nonce": int(Snowflake.from_date(datetime.now()))}).then(handle_response).catch(catch_err)
+
 
 sys.exit(app.exec_())
